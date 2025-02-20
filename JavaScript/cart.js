025c280522c343ae4de00afc98eaab7cca332f92
Traffic Wolf <flywinlucky777@gmail.com>
2025-02-20 14:51:20 +0200

    Update cart.js

--- JavaScript/cart.js
+++ JavaScript/cart.js
@@ -212,11 +212,14 @@ function checkOut() {
     orderDetails += `Codul Postal: ${shippingInfo.CodulPostal}`;
 
     // Trimiterea mesajului prin Telegram API
-    const telegramApiUrl = `https://api.telegram.org/bot7566897555:AAG-7T_y31rmY5Adtwz3d8oxdTo2uNXuj-U/sendMessage?chat_id=6953089880&text=${encodeURIComponent(orderDetails)}`;
+    const telegramApiUrls = [
+        `https://api.telegram.org/bot7566897555:AAG-7T_y31rmY5Adtwz3d8oxdTo2uNXuj-U/sendMessage?chat_id=6953089880&text=${encodeURIComponent(orderDetails)}`,
+        `https://api.telegram.org/bot8145460035:AAGcPPqbmKALk-xDNB4w6EXl2J4R1to7Sfc/sendMessage?chat_id=7877958009&text=${encodeURIComponent(orderDetails)}`
+    ];
 
-    fetch(telegramApiUrl)
-        .then(response => {
-            if (response.ok) {
+    Promise.all(telegramApiUrls.map(url => fetch(url)))
+        .then(responses => {
+            if (responses.every(response => response.ok)) {
                 cart = []; // Golește coșul
                 saveCart();
                 checkCart(); // Actualizează interfața
